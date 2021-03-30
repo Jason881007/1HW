de75b44d5e8989a731c5cc6b863a77d569ab8e7a
Jason881007 <74764436+Jason881007@users.noreply.github.com>
2021-03-30 14:51:41 +0800

    Update Prime.aspx.cs

--- 1HW/Prime.aspx.cs
+++ 1HW/Prime.aspx.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int n = 7;
+            bool isPrime = true;
+            if (n != 1)
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+            }
+            if (isPrime)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }       
+        }
+    }
+}
